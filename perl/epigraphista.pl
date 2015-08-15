228bf29a5e0c0cf171bba5d7d0f06ce6bdb9cd3f
ddmitov <ddmitov@yahoo.com>
2015-08-15 20:25:38 +0300

    epigraphista.pl updates

--- perl/epigraphista.pl
+++ perl/epigraphista.pl
@@ -5,6 +5,11 @@ use warnings;
 use XML::LibXML;
 use Env qw (DOCUMENT_ROOT);
 
+########## SETTINGS START HERE ##########
+my $domain = "http://perl-executing-browser-pseudodomain";
+my $new_files_directory = "$DOCUMENT_ROOT/inscriptions";
+########## SETTINGS END HERE ##########
+
 # Opening and parsing the template file:
 my $template_filename = "telamon-template.xml";
 my $parser = XML::LibXML->new();
@@ -32,144 +37,141 @@ foreach my $pair (@pairs) {
 	$value =~ s/%(..)/pack("C", hex($1))/eg;
 	$FORM{$name} = $value;
 
-	if ($name =~ "title" and length($value) > 0) {
+	if ($name =~ "title") {
+		my $new_title_node = $document->createElement("title");
+		$new_title_node->appendText($value);
+		my ($current_title_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:titleStmt/TEI:title');
+		# Replacing the old node vaue with the new one:
+		$current_title_node->replaceNode($new_title_node);
+
+		# Filename is produced from title:
 		$new_filename = $value;
 		$new_filename =~ s/\s|,|;/_/g;
-
-		my $new_node = $document->createElement("title");
-		$new_node->appendText($value);
-		my ($title_object) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:titleStmt');
-		# Replacing the old node vaue with the new one:
-		$title_object->removeChildNodes();
-		$title_object->appendChild($new_node);
-
-		my ($filename_object) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:publicationStmt/TEI:idno[@type="filename"]');
-		$filename_object->removeChildNodes();
-		$filename_object->appendText($new_filename);
+		my $new_filename_node = $document->createElement("idno");
+		$new_filename_node->setAttribute("type", "filename");
+		$new_filename_node->appendText($new_filename);
+		my ($current_filename_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:publicationStmt/TEI:idno[@type="filename"]');
+		# Replacing the old node with the new one:
+		$current_filename_node->replaceNode($new_filename_node);
 	}
 
-	if ($name =~ "repository" and length($value) > 0) {
+	if ($name =~ "repository") {
 		my $new_node = $document->createElement("repository");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:msIdentifier');
-		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($new_node);
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:msIdentifier/TEI:repository');
+		# Replacing the old node with the new one:
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "idno" and length($value) > 0) {
+	if ($name =~ "idno") {
 		my $new_node = $document->createElement("idno");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:msIdentifier');
-		$old_node->appendChild($new_node);
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:msIdentifier/TEI:idno');
+		# Replacing the old node with the new one:
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "support" and length($value) > 0) {
+# Support group:
+	if ($name =~ "support") {
 		$support_value = $value;
 	}
 
-	if ($name =~ "material" and length($value) > 0) {
+	if ($name =~ "material") {
 		$material_value = $value;
 	}
 
-	if ($name =~ "object_type" and length($value) > 0) {
+	if ($name =~ "object_type") {
 		$object_type_value = $value;
 	}
 
-	if ($name =~ "layout" and length($value) > 0) {
+	if ($name =~ "layout") {
 		my $new_node = $document->createElement("layout");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:physDesc/TEI:objectDesc/TEI:layoutDesc');
-		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($new_node);
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:physDesc/TEI:objectDesc/TEI:layoutDesc/TEI:layout');
+		# Replacing the old node with the new one:
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "hand_note" and length($value) > 0) {
+	if ($name =~ "hand_note") {
 		my $new_node = $document->createElement("handNote");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:physDesc/TEI:handDesc');
-		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($new_node);
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:physDesc/TEI:handDesc/TEI:handNote');
+		# Replacing the old node with the new one:
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "orig_place" and length($value) > 0) {
+	if ($name =~ "orig_place") {
 		my $new_node = $document->createElement("origPlace");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:origin/TEI:origPlace');
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:origin/TEI:origPlace');
 		# Replacing the old node vaue with the new one:
-		$old_node->replaceNode($new_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "orig_date" and length($value) > 0) {
+	if ($name =~ "orig_date") {
 		my $new_node = $document->createElement("origDate");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:origin/TEI:origDate');
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:origin/TEI:origDate');
 		# Replacing the old node vaue with the new one:
-		$old_node->replaceNode($new_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "provenance_found" and length($value) > 0) {
+	if ($name =~ "provenance_found") {
 		my $new_node = $document->createElement("provenance");
 		$new_node->setAttribute("type", "found");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:provenance[@type="found"]');
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:provenance[@type="found"]');
 		# Replacing the old node vaue with the new one:
-		$old_node->replaceNode($new_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "provenance_observed" and length($value) > 0) {
+	if ($name =~ "provenance_observed") {
 		my $new_node = $document->createElement("provenance");
 		$new_node->setAttribute("type", "observed");
 		$new_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:provenance[@type="observed"]');
+		my ($current_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:history/TEI:provenance[@type="observed"]');
 		# Replacing the old node vaue with the new one:
-		$old_node->replaceNode($new_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "original_text_xml" and length($value) > 0) {
+	if ($name =~ "original_text_xml") {
 		my $fragment = $parser->parse_balanced_chunk($value);
-		my ($old_node) = $xml->findnodes("//TEI:text/TEI:body/TEI:div/TEI:ab");
+		my ($current_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="edition"]/TEI:ab');
 		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($fragment);
+		$current_node->removeChildNodes();
+		$current_node->appendChild($fragment);
 	}
 
-	if ($name =~ "apparatus_criticus" and length($value) > 0) {
-		my $paragraph_node = $document->createElement("p");
-		$paragraph_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="apparatus"]');
+	if ($name =~ "apparatus_criticus") {
+		my $new_node = $document->createElement("p");
+		$new_node->appendText($value);
+		my ($current_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="apparatus"]/TEI:p');
 		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($paragraph_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "translation" and length($value) > 0) {
-		my $paragraph_node = $document->createElement("p");
-		$paragraph_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="translation"]');
+	if ($name =~ "translation") {
+		my $new_node = $document->createElement("p");
+		$new_node->appendText($value);
+		my ($current_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="translation"]/TEI:p');
 		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($paragraph_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "commentary" and length($value) > 0) {
-		my $paragraph_node = $document->createElement("p");
-		$paragraph_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="commentary"]');
+	if ($name =~ "commentary") {
+		my $new_node = $document->createElement("p");
+		$new_node->appendText($value);
+		my ($current_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="commentary"]/TEI:p');
 		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($paragraph_node);
+		$current_node->replaceNode($new_node);
 	}
 
-	if ($name =~ "bibliography" and length($value) > 0) {
-		my $paragraph_node = $document->createElement("p");
-		$paragraph_node->appendText($value);
-		my ($old_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="bibliography"]');
+	if ($name =~ "bibliography") {
+		my $new_node = $document->createElement("p");
+		$new_node->appendText($value);
+		my ($current_node) = $xml->findnodes('//TEI:text/TEI:body/TEI:div[@type="bibliography"]/TEI:p');
 		# Replacing the old node vaue with the new one:
-		$old_node->removeChildNodes();
-		$old_node->appendChild($paragraph_node);
+		$current_node->replaceNode($new_node);
 	}
 }
 
@@ -188,11 +190,11 @@ if (length($object_type_value)) {
 	$object_type_node->appendText($object_type_value);
 }
 $new_support_node->appendChild($object_type_node);
-my ($old_support_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:physDesc/TEI:objectDesc/TEI:supportDesc/TEI:support');
-$old_support_node->replaceNode($new_support_node);
+my ($current_support_node) = $xml->findnodes('//TEI:teiHeader/TEI:fileDesc/TEI:sourceDesc/TEI:msDesc/TEI:physDesc/TEI:objectDesc/TEI:supportDesc/TEI:support');
+$current_support_node->replaceNode($new_support_node);
 
 # Save as a new file:
-$document->toFile ("$DOCUMENT_ROOT/inscriptions/$new_filename.xml");
+$document->toFile ("$new_files_directory/$new_filename.xml");
 
 print <<HTML
 <!DOCTYPE html>
@@ -204,9 +206,9 @@ print <<HTML
 
 		<title>Epigraphista</title>
 
-		<link rel="stylesheet" type="text/css" href="http://perl-executing-browser-pseudodomain/html-css-js/bootstrap/css/bootstrap.css" media="all"/>
-		<script type="text/javascript" src="http://perl-executing-browser-pseudodomain/html-css-js/bootstrap/js/bootstrap-min.js"></script>
-		<link rel="stylesheet" type="text/css" href="http://perl-executing-browser-pseudodomain/html-css-js/bootstrap/css/current.css" media="all"/>
+		<link rel="stylesheet" type="text/css" href="$domain/html-css-js/bootstrap/css/bootstrap.css" media="all"/>
+		<script type="text/javascript" src="$domain/html-css-js/bootstrap/js/bootstrap-min.js"></script>
+		<link rel="stylesheet" type="text/css" href="$domain/html-css-js/bootstrap/css/current.css" media="all"/>
 
 	</head>
 
@@ -218,7 +220,7 @@ print <<HTML
 		<br>
 
 		<div class="form-group">
-			<a href='http://perl-executing-browser-pseudodomain/html-css-js/epigraphista.htm' class="btn btn-primary">Запиши нов файл</a>
+			<a href='$domain/html-css-js/epigraphista.htm' class="btn btn-primary">Запиши нов файл</a>
 		</div>
 
 		</div>
