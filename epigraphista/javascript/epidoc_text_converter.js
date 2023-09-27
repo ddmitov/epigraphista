6eaff690eb1652b8e077593b9ce06f860b942a76
Dimitar D. Mitov <ddmitov@gmail.com>
2023-09-27 20:47:50 +0300

    regular expressions update

--- epigraphista/javascript/epidoc_text_converter.js
+++ epigraphista/javascript/epidoc_text_converter.js
@@ -34,260 +34,99 @@ const unicodeBlocks =
   '\uff00-\uffef' // Halfwidth and Fullwidth Forms
 
 function convertLeidenToEpidoc (text) {
-  // ///////////////////////// //
-  // DOUBLE BRACKET ENCLOSURES //
-  // ///////////////////////// //
+  // ////////////////////////// //
+  // ANGLED BRACKETS ENCLOSURES //
+  // ////////////////////////// //
 
-  // <<exemplum>> //
-  // visual explanation:
-  // https://regexper.com/#%3C%3C%28%28%3F%3A%5Ba-z%5D%7Cs%29%2B%29%3E%3E
-  text = text.replace(
-    RegExp('<<((?:[' + unicodeBlocks + ']|s)+)>>', 'g'),
-    '<add place="overstrike">$1</add>'
-  )
-
-  // [[exemplum]] //
-  // visual explanation:
-  // https://regexper.com/#%5C%5B%5C%5B%28%28%3F%3A%5Ba-z%5D%7Cs%29%2B%29%5C%5D%5C%5D
-  text = text.replace(
-    RegExp('\\[\\[((?:[' + unicodeBlocks + ']|s)+)\\]\\]', 'g'),
-    '<del rend="erasure">$1</del>'
-  )
-
-  // ((exemplum)) //
-  // visual explanation:
-  // https://regexper.com/#%5C%28%5C%28%28%28%3F%3A%5Ba-z%5D%7Cs%29%2B%29%5C%29%5C%29
-  text = text.replace(
-    RegExp('\\(\\(((?:[' + unicodeBlocks + ']|s)+)\\)\\)', 'g'),
-    '<g type="$1"/>'
-  )
-
-  // ///////////////////////// //
-  // SINGLE BRACKET ENCLOSURES //
-  // ///////////////////////// //
+  // The following regular expression must be executed before all other!
 
   // <exemplum> //
   // visual explanation:
-  // https://regexper.com/#%28%5B%5E%3C%5D%29%3C%28%28%3F%3A%5Ba-z%5D%7Cs%29%2B%29%3E%28%5B%5E%3E%5D%29
+  // https://regexper.com/#%3C%28%5Ba-z%5D%7B1%2C%7D%29%3E
   text = text.replace(
-    RegExp('([^<])<((?:[' + unicodeBlocks + ']|s)+)>([^>])', 'g'),
-    '$1<supplied reason="omitted">$2</supplied>$3'
+    RegExp('<([' + unicodeBlocks + ']{1,})>', 'g'),
+    '<supplied reason="omitted">$1</supplied>'
   )
+
+  // /////////////////// //
+  // BRACKETS ENCLOSURES //
+  // /////////////////// //
+
+  // ((exemplum)) //
+  // visual explanation:
+  // https://regexper.com/#%5C%28%5C%28%28%5Ba-z%5D%7B1%2C%7D%29%5C%29%5C%29
+  text = text.replace(
+    RegExp('\\(\\(([' + unicodeBlocks + ']{1,})\\)\\)', 'g'),
+    '<expan><ex>$1</ex></expan>'
+  )
+
+  // ////////////////////////// //
+  // SQUARE BRACKETS ENCLOSURES //
+  // ////////////////////////// //
+
+  // [[exemplum]] //
+  // visual explanation:
+  // https://regexper.com/#%5C%5B%5C%5B%28%5Ba-z%5D%7B1%2C%7D%29%5C%5D%5C%5D
+  text = text.replace(
+    RegExp('\\[\\[([' + unicodeBlocks + ']{1,})\\]\\]', 'g'),
+    '<del rend="erasure">$1</del>'
+  )
+
+  // [ca.2]  //
+  // visual explanation:
+  // https://regexper.com/#%5C%5Bca%5C.%28%5B%5Cd%5D%29%7B1%2C%7D%5C%5D
+  text = text.replace(
+    /\[ca\.([\d]){1,}\]/g,
+    '<gap reason="lost" quantity="$1" unit="character" precision="low"/>'
+  )
+
+  // //////////////////////// //
+  // CURLY BRACKETS ENCLOSURE //
+  // //////////////////////// //
 
   // {exemplum} //
   // visual explanation:
-  // https://regexper.com/#%7B%28%28%3F%3A%5Ba-z%5D%7Cs%29%2B%29%7D
+  // https://regexper.com/#%5C%7B%28%5Ba-z%5D%7B1%2C%7D%29%5C%7D
   text = text.replace(
-    RegExp('{((?:[' + unicodeBlocks + ']|s)+)}', 'g'),
-    '<sic>$1</sic>'
+    RegExp('\\{([' + unicodeBlocks + ']{1,})\\}', 'g'),
+    '<surplus>$1</surplus>'
   )
 
-  // [c.2] //
+  // //////////// //
+  // OTHER MARKUP //
+  // //////////// //
+
+  // /*!*/ //
   // visual explanation:
-  // https://regexper.com/#%5C%5Bc%5C.%28%28%3F%3A%5Cd%29%2B%29%5C%5D
+  // https://regexper.com/#%5C%2F%5C*!%5C*%5C%2F
+  text = text.replace(/\/\*!\*\//g, '<note>!</note>')
+
+  // /*sic*/ //
+  // visual explanation:
+  // https://regexper.com/#%5C%2F%5C*sic%5C*%5C%2F
+  text = text.replace(/\/\*sic\*\//g, '<note>sic</note>')
+
+  // vac.2 //
+  // visual explanation:
+  // https://regexper.com/#%28%5B%5Cn%5Cs%5D%7B1%7D%29vac%5C.%28%5B%5Cd%5D%7B1%2C%7D%29%28%5B%5Cs%5Cn%5D%7B1%7D%29
   text = text.replace(
-    /\[c\.((?:\d)+)\]/g,
-    '<gap reason="lost" extent="$1" unit="character"/>'
+    /([\n\s]{1})vac\.([\d]{1,})([\s\n]{1})/g,
+    '$1<space quantity="$2" unit="character"/>$3'
   )
 
-  // [-] //
+  // vac.? //
   // visual explanation:
-  // https://regexper.com/#%5C%5B-%5C%5D
+  // https://regexper.com/#%28%5B%5Cn%5Cs%5D%7B1%7D%29vac%5C.%5C%3F%28%5B%5Cs%5Cn%5D%7B1%7D%29
   text = text.replace(
-    /\[-\]/g,
-    '<name><gap reason="lost" extent="unknown" unit="character"/></name>'
+    /([\n\s]{1})vac\.\?([\s\n]{1})/g,
+    '$1<space extent="unknown" unit="character"/>$2'
   )
 
-  // [- -] //
-  // visual explanation:
-  // https://regexper.com/#%28%5E%7C%5Cn%29%7B1%7D%28%5Cs%29%7B0%2C%7D%5C%5B-%5Cs-%5C%5D%28%5Cs%29%7B0%2C%7D%28%5Cn%7C%24%29%7B1%7D
+  // *leaf* //
   text = text.replace(
-    /(^|\n){1}(\s){0,}\[-\s-\](\s){0,}(\n|$){1}/g,
-    '$1<gap reason="lost" extent="1" unit="line"/>\n'
+    /\*leaf\*/g,
+    '<g type="leaf"/>'
   )
-
-  // [- -] ? //
-  // visual explanation:
-  // https://regexper.com/#%28%5E%7C%5Cn%29%7B1%7D%28%5B%5Cs%5D%29%7B0%2C%7D%5C%5B-%5Cs-%5C%5D%5Cs%5C%3F%28%5Cn%7C%24%29%7B1%7D
-  text = text.replace(
-    /(^|\n){1}([\s]){0,}\[-\s-\]\s\?(\n|$){1}/g,
-    '$1<gap reason="lost" extent="unknown" unit="line"/>'
-  )
-
-  // [--] //
-  // visual explanation:
-  // https://regexper.com/#%28%5B%5E%5B%5D%29%5C%5B%28%5B-%5D%2B%29%5C%5D%28%5B%5E%5C%5D%5D%29
-  const lostCharactersRegExp = /([^[])\[([-]+)\]([^\]])/
-
-  while (text.match(lostCharactersRegExp)) {
-    text = text.replace(
-      lostCharactersRegExp,
-      '$1<gap reason="lost" extent="' +
-        RegExp.$2.length +
-        '" unit="character"/>$3'
-    )
-  }
-
-  // [..] //
-  // visual explanation:
-  // https://regexper.com/#%5C%5B%28%5B.%5D%2B%29%5C%5D
-  const lostRegExp = /\[([.]+)\]/
-
-  while (text.match(lostRegExp)) {
-    // Brackets are not counted!
-    text = text.replace(
-      lostRegExp,
-      '<gap reason="lost" extent="' +
-        (RegExp.lastMatch.length - 2) +
-        '" unit="character"/>'
-    )
-  }
-
-  // (!) //
-  text = text.replace(/\(!\)/g, '<note>!</note>')
-
-  // (scil. exemplum) //
-  // visual explanation:
-  // https://regexper.com/#%5C%28scil%5C.%5Cs%28%28%3F%3A%5Ba-z%5D%7Cs%29%2B%29%5C%29
-  text = text.replace(
-    RegExp('\\(scil\\.\\s((?:[' + unicodeBlocks + ']|s)+)\\)', 'g'),
-    '<supplied reason="subaudible">$1</supplied>'
-  )
-
-  // exemplum(- -) //
-  // visual explanation:
-  // https://regexper.com/#%28%5CS%2B%29%5C%28%28-%5Cs*%29%2B%5C%29
-  text = text.replace(/(\S+)\((-\s*)+\)/g, '<abbr>$1</abbr>')
-
-  // (- -) //
-  // TO DO
-  text = text.replace(
-    /\(((?:(-)|\s)+)\)/g,
-    '<gap reason="omitted" extent="unknown" unit="character"/>'
-  )
-
-  // (vac.2)  //
-  // (vac. 2) //
-  // visual explanation:
-  // https://regexper.com/#%5C%28vac%5C.%28%5Cs%29%7B0%2C%7D%28%5Cd%29%7B1%2C%7D%5C%29%28%5B%5Cs%5Cn%5D%7C%24%29%7B0%2C1%7D
-  text = text.replace(
-    /\(vac\.(\s){0,}(\d){1,}\)([\s\n]|$){0,1}/g,
-    '<space extent="$2" unit="character"/>$3'
-  )
-
-  // (vac.?) //
-  // visual explanation:
-  // https://regexper.com/#%28%5E%7C%5B%5Cn%5Cs%5D%29%7B1%7D%28%5C%28vac%5C.%5C%3F%5C%29%29%28%5B%5Cs%5Cn%5D%7C%24%29%7B0%2C1%7D
-  text = text.replace(
-    /(^|[\n\s]){1}(\(vac\.\?\))([\s\n]|$){0,1}/g,
-    '<space extent="unknown" unit="character"/>$3'
-  )
-
-  // vac.  //
-  // vac   //
-  // vacat //
-  // visual explanation:
-  // https://regexper.com/#%28%5E%7C%5B%5Cn%5Cs%5D%29%7B1%7Dvac%28%5C.%29%7B0%2C1%7D%28at%29%7B0%2C1%7D%28%5B%5Cs%5Cn%5D%7C%24%29%7B1%7D
-  text = text.replace(
-    /(^|[\n\s]){1}vac(\.){0,1}(at){0,1}([\s\n]|$){1}/g,
-    '<space extent="unknown" unit="character"/>$4'
-  )
-
-  // (ex)em(plum) //
-  // visual explanation:
-  // https://regexper.com/#%5C%28%28%5Ba-z%5D%2B%29%5C%29%28%5Ba-z%5D%2B%29%5C%28%28%5Ba-z%5D%2B%29%5C%29
-  text = text.replace(
-    RegExp(
-      '\\(([' + unicodeBlocks + ']+)\\)' +
-      '([' + unicodeBlocks + ']+)' +
-      '\\(([' + unicodeBlocks + ']+)\\)',
-      'g'
-    ),
-    '<expan><ex>$1</ex><abbr>$2</abbr><ex>$3</ex></expan>'
-  )
-
-  // ex(em)plum //
-  // visual explanation:
-  // https://regexper.com/#%28%5Ba-z%5D%2B%29%5C%28%28%5Ba-z%5D%2B%29%5C%29%28%5Ba-z%5D%2B%29
-  text = text.replace(
-    RegExp(
-      '([' + unicodeBlocks + ']+)' +
-      '\\(([' + unicodeBlocks + ']+)\\)' +
-      '([' + unicodeBlocks + ']+)',
-      'g'
-    ),
-    '<expan><abbr>$1</abbr><ex>$2</ex><abbr>$3</abbr></expan>'
-  )
-
-  // (ex)emplum //
-  // visual explanation:
-  // https://regexper.com/#%5C%28%28%5Ba-z%5D%2B%29%5C%29%28%5Ba-z%5D%2B%29
-  text = text.replace(
-    RegExp(
-      '\\(([' + unicodeBlocks + ']+)\\)' + '([' + unicodeBlocks + ']+)',
-      'g'
-    ),
-    '<expan><ex>$1</ex><abbr>$2</abbr></expan>'
-  )
-
-  // ex(emplum) //
-  // visual explanation:
-  // https://regexper.com/#%28%5Ba-z%5D%2B%29%5C%28%28%5Ba-z%5D%2B%29%5C%29
-  text = text.replace(
-    RegExp(
-      '([' + unicodeBlocks + ']+)' + '\\(([' + unicodeBlocks + ']+)\\)',
-      'g'
-    ),
-    '<expan><abbr>$1</abbr><ex>$2</ex></expan>'
-  )
-
-  // (exemplum) //
-  // TO DO
-  text = text.replace(
-    RegExp('\\(((?:[' + unicodeBlocks + ']|s)+)\\)', 'g'),
-    '<expan><abbr><am><g type="symbol"/></am></abbr><ex>$1</ex></expan>'
-  )
-
-  // /////////////////////////// //
-  // SINGLE CHARACTER ENCLOSURES //
-  // /////////////////////////// //
-
-  // 'exemplum' //
-  // visual explanation:
-  // https://regexper.com/#%5C'%28%3F%3A%5Ba-z%5D%7C%5Cs%29%2B%5C'
-  text = text.replace(
-    RegExp('\'((?:[' + unicodeBlocks + ']|\\s)+)\'', 'g'),
-    '<add>$1</add>'
-  )
-
-  // ⌜exemplum⌝ //
-  // TOP LEFT CORNER - Unicode 231C  //
-  // TOP RIGHT CORNER - Unicode 231D //
-  // visual explanation:
-  // https://regexper.com/#%5C'%28%28%3F%3A%5Ba-z%5D%7C%5Cs%29%2B%29%5C'
-  text = text.replace(
-    RegExp('⌜(([' + unicodeBlocks + ']|\\s){1,})⌝', 'g'),
-    '<choice><sic>$1</sic><corr>$1</corr></choice>'
-  )
-
-  // ///////////// //
-  // OTHER MARKUPS //
-  // ///////////// //
-
-  // ++ //
-  const illegibleRegExp = /((\+)+)/
-
-  while (text.match(illegibleRegExp)) {
-    text = text.replace(
-      illegibleRegExp,
-      '<gap reason="illegible" extent="' +
-        RegExp.lastMatch.length +
-        '" unit="character"/>'
-    )
-  }
-
-  // ... //
-  text = text.replace(/\.\.\./g, '<gap reason="ellipsis"/>')
 
   // //////////// //
   // LINE NUMBERS //
@@ -310,36 +149,13 @@ function convertLeidenToEpidoc (text) {
 
 // Paste in the 'Inscription Text' textarea:
 
-// 1.  <<exemplum>>
-// 2.  [[exemplum]]
-// 3.  ((exemplum))
-// 4.  <exemplum>
-// 5.  {exemplum}
-// 6.  [c.2]
-// 7.  [-]
-// 8.  [--]
-// 9.  [..]
-// 10. (!)
-// 11. (scil. exemplum)
-// 12. exemplum(- -)
-// 13. (- -)
-// 14. (vac.2)
-// 15. (vac. 2)
-// 16. (vac.?)
-// 17. vac.
-// 18. vac
-// 19. vacat
-// 20. (ex)em(plum)
-// 21. ex(em)plum
-// 22. (ex)emplum
-// 23. ex(emplum)
-// 24. (exemplum)
-// 25. 'exemplum'
-// 26. ⌜exemplum⌝
-// 27. ++
-// 28. ...
-
-// Paste in the 'Inscription Text' textarea
-// without the comment mark and with nothing else on the line:
-// [- -]
-// [- -] ?
+// 1.  <exemplum>   //
+// 2.  ((exemplum)) //
+// 3.  [[exemplum]] //
+// 4.  [ca.2]       //
+// 5.  {exemplum}   //
+// 6.  /*!*/        //
+// 7.  /*sic*/      //
+// 8.  vac.2        //
+// 9.  vac.?        //
+// 10. *leaf*       //
