6bc573a9f518a7804cb69fdf2a8af6bab85d7c77
Dimitar D. Mitov <ddmitov@gmail.com>
2023-10-04 10:44:16 +0300

    regular expressions update

--- epigraphista/javascript/leiden_to_epidoc_converter.js
+++ epigraphista/javascript/leiden_to_epidoc_converter.js
@@ -10,16 +10,19 @@
 // https://sourceforge.net/projects/epidoc/files/OldFiles/chetc-js/r2/
 // https://cds.library.brown.edu/projects/chet-c/chetc.html
 
-// Some helpful utilities for the development of Regular Expressions:
-// https://regexr.com/
-// https://regexper.com/
-// https://pemistahl.github.io/grex-js/
+// All names and numbers of text patterns here follow the table
+// "Leiden/Leiden+ and EpiDoc Quick Reference (after Krummrey-Panciera)"
+// by Gabriel Bodard 2017-03-17 available at
+// https://svn.code.sf.net/p/epidoc/code/trunk/guidelines/msword/cheatsheet.pdf
 
-// Unicode Character Search:
-// https://www.fileformat.info/info/unicode/char/search.htm
+// For development and testing of Regular Expressions go to the RegExr tool at
+// https://regexr.com/
+
+// For visual representation of Regular Expressions go to the Regexper toool at
+// https://regexper.com/
 
 const unicodeBlocks =
-  '\u0041-\u007a' + // Basic Latin
+  'a-zA-Z' + // Basic Latin
   '\u0080-\u00ff' + // Latin-1 Supplement
   '\u0180-\u024f' + // Latin Extended-B
   '\u1e00-\u1eff' + // Latin Extended Additional
@@ -34,20 +37,24 @@ const unicodeBlocks =
   '\uff00-\uffef' // Halfwidth and Fullwidth Forms
 
 function convertLeidenToEpidoc (text) {
-  // Convert only non-zero length Leiden text:
+  // Convert only non-zero length text:
   if (text.length === 0) {
     return text
   }
 
-  // ////////////////////////// //
-  // ANGLED BRACKETS ENCLOSURES //
-  // ////////////////////////// //
+  // ///////////////////////////////// //
+  // ANGLED BRACKETS ENCLOSURE         //
+  //                                   //
+  // The following regular expression  //
+  // must be executed before all other //
+  // to avoid mismatching of XML tags! //
+  // ///////////////////////////////// //
 
-  // The following regular expression must be executed before all other!
-
-  // <exemplum> //
-  // visual explanation:
-  // https://regexper.com/#%3C%28%5Ba-z%5D%7B1%2C%7D%29%3E
+  // <ab>
+  // IX.2 "Omitted letters added by editor"
+  // Use the following Latin-only version
+  // for Regexper visualization at https://regexper.com/
+  // <([a-z]{1,})>
   text = text.replace(
     RegExp('<([' + unicodeBlocks + ']{1,})>', 'g'),
     '<supplied reason="omitted">$1</supplied>'
@@ -57,9 +64,11 @@ function convertLeidenToEpidoc (text) {
   // BRACKETS ENCLOSURES //
   // /////////////////// //
 
-  // ((exemplum)) //
-  // visual explanation:
-  // https://regexper.com/#%5C%28%5C%28%28%5Ba-z%5D%7B1%2C%7D%29%5C%29%5C%29
+  // ((ab))
+  // X.3 "Expansion of symbol"
+  // Use the following Latin-only version
+  // for Regexper visualization at https://regexper.com/
+  // \(\(([a-z]{1,})\)\)
   text = text.replace(
     RegExp('\\(\\(([' + unicodeBlocks + ']{1,})\\)\\)', 'g'),
     '<expan><ex>$1</ex></expan>'
@@ -69,19 +78,30 @@ function convertLeidenToEpidoc (text) {
   // SQUARE BRACKETS ENCLOSURES //
   // ////////////////////////// //
 
-  // [[exemplum]] //
-  // visual explanation:
-  // https://regexper.com/#%5C%5B%5C%5B%28%5Ba-z%5D%7B1%2C%7D%29%5C%5D%5C%5D
+  // [ab]
+  // VIII.1 "Characters lost but restored"
+  // Use the following Latin-only version
+  // for Regexper visualization at https://regexper.com/
+  // (^|[^[]{1,1})\[([a-z]{1,})\]([^\]]{1,1}|$)
   text = text.replace(
-    RegExp('\\[\\[([' + unicodeBlocks + ']{1,})\\]\\]', 'g'),
+    RegExp(
+      '(^|[^[]{1,1})\\[([' + unicodeBlocks + ']{1,})\\]([^\\]]{1,1}|$)',
+      'g'
+    ),
+    '$1<supplied reason="lost">$2</supplied>$3'
+  )
+
+  // [[ab]]
+  // V.1 "Erased"
+  text = text.replace(
+    /\[\[([^[\]]{1,})\]\]/g,
     '<del rend="erasure">$1</del>'
   )
 
-  // [ca.2]  //
-  // visual explanation:
-  // https://regexper.com/#%5C%5Bca%5C.%28%5B%5Cd%5D%29%7B1%2C%7D%5C%5D
+  // [ca.2]
+  // VIII.3 "Lacuna, approximate extent"
   text = text.replace(
-    /\[ca\.([\d]){1,}\]/g,
+    /\[ca\.([\d]{1,})\]/g,
     '<gap reason="lost" quantity="$1" unit="character" precision="low"/>'
   )
 
@@ -89,51 +109,54 @@ function convertLeidenToEpidoc (text) {
   // CURLY BRACKETS ENCLOSURE //
   // //////////////////////// //
 
-  // {exemplum} //
-  // visual explanation:
-  // https://regexper.com/#%5C%7B%28%5Ba-z%5D%7B1%2C%7D%29%5C%7D
+  // {ab}
+  // IX.1 "Superfluous letters suppressed by editor"
+  // Use the following Latin-only version
+  // for Regexper visualization at https://regexper.com/
+  // \{([a-z]{1,})\}
   text = text.replace(
     RegExp('\\{([' + unicodeBlocks + ']{1,})\\}', 'g'),
     '<surplus>$1</surplus>'
   )
 
+  // //////////////// //
+  // SLASH ENCLOSURES //
+  // //////////////// //
+
+  // /*!*/
+  // XI.1 "Editor's note"
+  text = text.replace(/\/\*!\*\//g, '<note>!</note>')
+
+  // /*sic*/
+  // XI.1 "Editor's note"
+  text = text.replace(/\/\*sic\*\//g, '<note>sic</note>')
+
   // //////////// //
   // OTHER MARKUP //
   // //////////// //
 
-  // /*!*/ //
-  // visual explanation:
-  // https://regexper.com/#%5C%2F%5C*!%5C*%5C%2F
-  text = text.replace(/\/\*!\*\//g, '<note>!</note>')
-
-  // /*sic*/ //
-  // visual explanation:
-  // https://regexper.com/#%5C%2F%5C*sic%5C*%5C%2F
-  text = text.replace(/\/\*sic\*\//g, '<note>sic</note>')
-
-  // vac.2 //
-  // visual explanation:
-  // https://regexper.com/#%28%5B%5Cn%5Cs%5D%7B1%7D%29vac%5C.%28%5B%5Cd%5D%7B1%2C%7D%29%28%5B%5Cs%5Cn%5D%7B1%7D%29
+  // vac.2
+  // XI.2 "Space left on stone"
   text = text.replace(
     /([\n\s]{1})vac\.([\d]{1,})([\s\n]{1})/g,
     '$1<space quantity="$2" unit="character"/>$3'
   )
 
-  // vac.? //
-  // visual explanation:
-  // https://regexper.com/#%28%5B%5Cn%5Cs%5D%7B1%7D%29vac%5C.%5C%3F%28%5B%5Cs%5Cn%5D%7B1%7D%29
+  // vac.?
+  // XI.3 "Space on stone, extent unknown"
   text = text.replace(
     /([\n\s]{1})vac\.\?([\s\n]{1})/g,
     '$1<space extent="unknown" unit="character"/>$2'
   )
 
-  // *leaf* //
+  // *leaf*
+  // "Symbol"
   text = text.replace(
     /\*leaf\*/g,
     '<g type="leaf"/>'
   )
 
-  // //////////// //
+  // //////////// ///
   // LINE NUMBERS //
   // //////////// //
 
@@ -152,15 +175,16 @@ function convertLeidenToEpidoc (text) {
 // TESTING STRINGS //
 // /////////////// //
 
-// Paste in the 'Inscription Text' textarea:
+// Paste in the 'Leiden+ Inscription Text' textarea:
 
-// 1.  <exemplum>   //
-// 2.  ((exemplum)) //
-// 3.  [[exemplum]] //
-// 4.  [ca.2]       //
-// 5.  {exemplum}   //
-// 6.  /*!*/        //
-// 7.  /*sic*/      //
-// 8.  vac.2        //
-// 9.  vac.?        //
-// 10. *leaf*       //
+// 1.  <ab>    //
+// 2.  ((ab))  //
+// 3.  [[ab]]  //
+// 4   [ab]    //
+// 5.  [ca.2]  //
+// 6.  {ab}    //
+// 7.  /*!*/   //
+// 8.  /*sic*/ //
+// 9.  vac.2   //
+// 10.  vac.?  //
+// 11. *leaf*  //
